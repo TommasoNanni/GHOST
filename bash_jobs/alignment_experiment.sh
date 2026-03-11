c221ae3430ddad839137ca42039d9934d707807b
TommasoNanni <tnanni@student.ethz.ch>
2026-03-11 12:35:09 +0100

    added bash job for alignment and edited the one for rich pipeline

--- bash_jobs/alignment_experiment.sh
+++ bash_jobs/alignment_experiment.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-#SBATCH --time=20:00:00
+#SBATCH --time=1:00:00
 #SBATCH --account=ls_polle
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=15
 #SBATCH --mem-per-cpu=8G
-#SBATCH --gpus=2
-#SBATCH --gres=gpumem:40G 
-#SBATCH --job-name=rich_pipeline
+#SBATCH --job-name=alignment_experiment
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
-#SBATCH --mail-type=ALL          
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=tnanni@ethz.ch
 
 set -euo pipefail
@@ -17,20 +15,15 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd /cluster/project/cvg/students/tnanni/ghost
 
-echo "=== GPU STATUS ==="
-nvidia-smi
-echo "========================="
-
 echo "Job ID:       $SLURM_JOB_ID"
 echo "Node:         $SLURMD_NODENAME"
-echo "GPUs:         $SLURM_GPUS"
 echo "Start:        $(date)"
 echo "Working dir:  $SCRIPT_DIR"
 echo ""
 
 
 # Run via pixi (activates the correct conda env automatically)
-pixi run python -m scripts.rich_pipeline
+CONDA_OVERRIDE_CUDA=12.6 pixi run python -m scripts.alignment_experiments
 
 echo ""
 echo "Done: $(date)"
